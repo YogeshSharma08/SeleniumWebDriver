7d273b9a17e6e7b381f9916c20b90d8f6eb978b2
Yogesh <YogeshS@proteantech.in>
2025-07-18 00:28:06 -0700

    Asian development changes

--- src/GitExample.java
+++ src/GitExample.java
@@ -1,14 +1,20 @@
+import org.testng.annotations.Test;
 
 public class GitExample {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	@Test	
+	public  void first()
+	{
+		System.out.println("develop branch new change");
+	// TODO Auto-generated method stub
 		
 		System.out.println("first changed");
 		
 		System.out.println("Second changed");
 		System.out.println("Third changed");
 		
+		
+	
 	}
-
 }
---
+++ src/TestNgTutorials/GitTestNG.java
@@ -0,0 +1,21 @@
+package TestNgTutorials;
+
+import org.testng.annotations.Test;
+
+public class GitTestNG {
+	
+	
+	@Test	
+	public  void first()
+	{
+		System.out.println("develop branch first change");
+	}
+	
+	
+	@Test	
+	public  void second()
+	{
+		System.out.println("develop branch second change");
+	}
+
+}
